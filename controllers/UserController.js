67d790b552fe7585a1e88f58cf0cdab243493463
Luis Henrique Martins dos Santos Bonfim <luis.bonfim@iesde.com.br>
2023-04-04 17:50:44 -0300

    sign up adjust

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,29 +5,21 @@ const jwt = require('jsonwebtoken');
 function signUp(req, res) {
 
     bcryptjs.genSalt(10, function (err, salt) {
-        bcryptjs.hash(req.body.password, salt, function (err, hasgh) {
+        bcryptjs.hash(req.body.passoword, salt, function (err, hash) {
             const user = {
                 name: req.body.name,
                 email: req.body.email,
-                password: hash,
+                passoword: hash,
             }
-        });
-    });
-
-
-    const user = {
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    }
-
-    models.User.create(user).then(result => {
-        res.status(201).json({
-            message: 'User created successfully',
-        });
-    }).catch(error => {
-        res.status(500).json({
-            message: "Something went wrong"
+            models.User.create(user).then(result => {
+                res.status(201).json({
+                    message: 'User created successfully',
+                });
+            }).catch(error => {
+                res.status(500).json({
+                    message: "Something went wrong"
+                });
+            });
         });
     });
 }
